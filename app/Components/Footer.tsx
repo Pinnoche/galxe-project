f03d3a510586b89c1c73d45db4110ab06b8dba61
ajani-m1704425 <88562907+ajani-m1704425@users.noreply.github.com>
2023-11-01 04:44:53 +0100

    Footer links Added

--- app/Components/Footer.tsx
+++ app/Components/Footer.tsx
@@ -5,24 +5,32 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 async function Footer() {
-    const file = await fs.readFile(process.cwd() + '../json/footer.json', 'utf8');
+    const file = await fs.readFile(process.cwd() + '/app/json/footer.json', 'utf8');
     const data = JSON.parse(file);
     
     return (
-        <footer className='bg-my-color px-7'>
-            <div className='py-12 flex'>
-                <div className='mb-12'>
-                    <img className='h-5' src="https://b.galxestatic.com/w/s/76c4073/img/new-logo.bc3e2ed.svg" alt="Galxe Logo" />
+        <footer className='bg-my-color px-7 xl:px-20'>
+            <div className='py-12 flex flex-wrap xl:py-20'>
+                <div className='mb-12 w-full xl:w-fit'>
+                    <img className='h-5' src={data.logo} alt="Galxe Logo" />
                     <p className='mt-5 max-w-lg text-left text-sm font-normal text-footer-color'>
-                        Galxe is the leading platform for building Web3 community. With over 11 million unique users, Galxe has
-                        propelled the growth of Optimism, Polygon, Arbitrum, and more than 3000 partners with reward-based loyalty programs.
+                        {data.content}
                     </p>
                     <button className='w-fit mt-6 py-3 px-7 border border-footer-button-color  text-footer-button-color rounded-lg font-bold text-base '>
-                        
-                    </button>
-                    <div className="flex">
+                        {data.latest}
+                    </button>                    
+                </div>
+                <div className="flex flex-wrap w-full xl:">
+                    {data.link_head.map((head:String,index:number) => (
+                        <div className="w-1/2 mb-4 md:w-1/4 lg:w-1/5">
+                            <p className="text-sm font-normal text-white mb-3 font-bold ">{head}</p>
+                            {data.links[index].map((link: String)=>(
+                                <a href="/" className="block text-sm font-normal text-footer-color mb-3 ">{link}</a>
+                            ))}
+                        </div>
+                    )    
+                    )}
 
-                    </div>
                 </div>
             </div>            
         </footer>
