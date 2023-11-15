61d7d2d5e708705703e52252d47e33644a776e8f
Pinnoche <ptime552@gmail.com>
2023-11-15 17:22:13 -0600

    fresh

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -2,11 +2,13 @@ import React, { FC, useState } from "react";
 import Section1 from "./Section1";
 import Section5 from "./Section5";
 import Section10 from "./section10";
+import Section2 from "./Section2";
 
 const Page = () => {
   return (
     <>
       <Section1 />
+      <Section2 />
       <Section5 />
       <Section10 />
     </>
