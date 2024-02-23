1d6de473a50b93ebc75c1948dfd9b144692270b0
Michael Ibitoye <michael303.mi@gmail.com>
2024-02-23 22:36:26 +0100

    responsive design configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,31 +7,57 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    screens: {
+      laptop: { max: "1023px" },
+      // => @media (max-width: 1023px) { ... }
+
+      tablet: { max: "767px" },
+      // => @media (max-width: 767px) { ... }
+
+      phone: { max: "639px" },
+      // => @media (max-width: 639px) { ... }
+
+      sm: "640px",
+      // => @media (min-width: 640px) { ... }
+
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      xl: "1280px",
+      // => @media (min-width: 1280px) { ... }
+
+      "2xl": "1536px",
+      // => @media (min-width: 1536px) { ... }
+    },
     extend: {
       fontFamily: {
         "mono-sans": ["var(--font-mono-sans)"],
       },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-        "sect3": "url('https://b.galxestatic.com/w/s/5f88ccb/img/recommendbg.1db2505.png')",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        sect3:
+          "url('https://b.galxestatic.com/w/s/5f88ccb/img/recommendbg.1db2505.png')",
       },
       backgroundColor: {
-        'my-color': '#060708'
-        
+        "my-color": "#060708",
       },
       backgroundPosition: {
-        "fifty": '50%',
+        fifty: "50%",
       },
       backgroundSize: {
-        'hundred': '100% 100%',
+        hundred: "100% 100%",
       },
       colors: {
-        'footer-color': '#ced3db',
-        'footer-button-color': '#0057ff',
-        'footer-light': '#9097a6',
-        'footer2-bgx': '#15171c'
-      }
+        "footer-color": "#ced3db",
+        "footer-button-color": "#0057ff",
+        "footer-light": "#9097a6",
+        "footer2-bgx": "#15171c",
+      },
     },
   },
   plugins: [],
