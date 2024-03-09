3d7ea05eea461db4a0b10122e9f8126f568c5105
Pinnoche <ptime552@gmail.com>
2024-03-08 18:40:51 -0700

    navbar

--- app/components/Navbar/index.tsx
+++ app/components/Navbar/index.tsx
@@ -15,22 +15,44 @@ export default function Navbar() {
   const [protocolList, setProtocolList] = useState(false);
   const [moreList, setMoreList] = useState(false);
   const [showMenu, setShowMenu] = useState(false);
-  const [winscreen, setWinscreen] = useState(window.innerWidth);
+  const [winscreen, setWinscreen] = useState(innerWidth);
+  const [scrollPercentage, setScrollPercentage] = useState(0);
 
   useEffect(() => {
-    window.onresize = () => {
-      setWinscreen(window.innerWidth);
+    onresize = () => {
+      setWinscreen(innerWidth);
     };
   }, [winscreen]);
 
-  console.log(winscreen);
+  const handleScroll = () => {
+    const doc = document.documentElement;
+    const scrollTop = (doc && doc.scrollTop) || 0;
+    const windowHeight = innerHeight;
+    const documentHeight = doc.scrollHeight;
 
-  console.log(exploreList);
+    const newScrollPercentage =
+      ((scrollTop + windowHeight) / documentHeight) * 100;
+    setScrollPercentage(newScrollPercentage);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
   return (
     <div
       className={`fixed top-0 bg-black w-full h-20 z-[7] ${
         showMenu == true ? "tablet:min-h-full" : "h-20"
+      } ${
+        showMenu == false
+          ? scrollPercentage <= 44
+            ? "bg-transparent backdrop-blur-sm"
+            : "bg-black"
+          : undefined
       }`}
     >
       <nav className="w-full h-full min-h-full tablet:overflow-auto no-scrollbar">
@@ -89,14 +111,16 @@ export default function Navbar() {
                   onMouseLeave={() => {
                     winscreen > 1024 ? setExploreList(false) : undefined;
                   }}
-                  onClick={() => {
-                    winscreen <= 1024
-                      ? setExploreList((prev) => !prev)
-                      : setExploreList(false);
-                  }}
                   className="relative tablet:border-b-[#1b1e24] tablet:border-b-[1px] tablet:pb-2 tablet:mb-4"
                 >
-                  <span className="flex items-center tablet:justify-between cursor-pointer tablet:hover:text-white">
+                  <span
+                    onClick={() => {
+                      winscreen <= 1024
+                        ? setExploreList((prev) => !prev)
+                        : setExploreList(false);
+                    }}
+                    className="flex items-center tablet:justify-between cursor-pointer tablet:hover:text-white"
+                  >
                     Explore
                     <AiFillCaretDown className="h-4 w-7 hover:text-blue-700 tablet:hover:text-white" />
                   </span>
@@ -133,13 +157,15 @@ export default function Navbar() {
                   onMouseLeave={() => {
                     winscreen > 1024 ? setGalxeList(false) : undefined;
                   }}
-                  onClick={() => {
-                    winscreen <= 1024
-                      ? setGalxeList((prev) => !prev)
-                      : setGalxeList(false);
-                  }}
                 >
-                  <span className="flex items-center tablet:justify-between cursor-pointer hover:text-blue-700 tablet:hover:text-white">
+                  <span
+                    onClick={() => {
+                      winscreen <= 1024
+                        ? setGalxeList((prev) => !prev)
+                        : setGalxeList(false);
+                    }}
+                    className="flex items-center tablet:justify-between cursor-pointer hover:text-blue-700 tablet:hover:text-white"
+                  >
                     Galxe ID <AiFillCaretDown className="h-4 w-7" />
                   </span>
                   {galxeList && (
@@ -169,13 +195,15 @@ export default function Navbar() {
                   onMouseLeave={() => {
                     winscreen > 1024 ? setProtocolList(false) : undefined;
                   }}
-                  onClick={() => {
-                    winscreen <= 1024
-                      ? setProtocolList((prev) => !prev)
-                      : setProtocolList(false);
-                  }}
                 >
-                  <span className="flex items-center tablet:justify-between cursor-pointer hover:text-blue-700 tablet:hover:text-white">
+                  <span
+                    onClick={() => {
+                      winscreen <= 1024
+                        ? setProtocolList((prev) => !prev)
+                        : setProtocolList(false);
+                    }}
+                    className="flex items-center tablet:justify-between cursor-pointer hover:text-blue-700 tablet:hover:text-white"
+                  >
                     Protocol <AiFillCaretDown className="h-4 w-7" />
                   </span>
                   <div
@@ -203,13 +231,15 @@ export default function Navbar() {
                   onMouseLeave={() => {
                     winscreen > 1024 ? setMoreList(false) : undefined;
                   }}
-                  onClick={() => {
-                    winscreen <= 1024
-                      ? setMoreList((prev) => !prev)
-                      : setMoreList(false);
-                  }}
                 >
-                  <span className="flex items-center cursor-pointer hover:text-blue-700 tablet:hover:text-white tablet:flex tablet:justify-between">
+                  <span
+                    onClick={() => {
+                      winscreen <= 1024
+                        ? setMoreList((prev) => !prev)
+                        : setMoreList(false);
+                    }}
+                    className="flex items-center cursor-pointer hover:text-blue-700 tablet:hover:text-white tablet:flex tablet:justify-between"
+                  >
                     More <AiFillCaretDown className="h-4 w-7" />
                   </span>
                   {moreList && (
@@ -256,7 +286,7 @@ export default function Navbar() {
                             <li>Apply for Your Space</li>
                           </ul>
                         </div>
-                        <div className="my-6 tablet:my-0">
+                        <div className="my-6 tablet:my-0 tablet:mb-8">
                           <ul className="list">
                             Disclaimer
                             <li className="mt-4">Terms of Service</li>
@@ -270,18 +300,18 @@ export default function Navbar() {
                   )}
                 </div>
               </div>
-              <div className="flex mr-4 ml-28 tablet:ml-4 tablet:mt-40 tablet:mb-8">
-                <button className="bg-gray-700 text-gray-400 rounded-full text-xs py-[0.4374rem] px-6 mr-4 hover:bg-gray-500 tablet:hidden max-w-16 max-h-8">
-                  Network
-                </button>
-                <button
-                  className={`bg-blue-700 rounded-full tablet:rounded-lg tablet:min-w-full tablet:min-h-[52px] tablet:text-base tablet:font-bold tablet:mb-0 py-[0.4374rem] px-6 tablet:py-[14px] tablet:px-[40px] text-xs text-white font-bold hover:bg-blue-500 max-h-8 ${
-                    showMenu == true ? "block" : "tablet:hidden"
-                  }`}
-                >
-                  Log in
-                </button>
-              </div>
+            </div>
+            <div className="flex mr-4 ml-28 tablet:mr-0 tablet:ml-4 tablet:mt-40 tablet:mb-0 tablet:fixed tablet:min-w-[95%] bottom-[1vh]">
+              <button className="bg-gray-700 text-gray-400 rounded-full text-xs py-[0.4374rem] px-6 mr-4 hover:bg-gray-500 tablet:hidden max-w-16 max-h-8">
+                Network
+              </button>
+              <button
+                className={`bg-blue-700 rounded-full tablet:rounded-lg tablet:min-w-full tablet:min-h-[52px] tablet:text-base tablet:font-bold tablet:mb-0 py-[0.4374rem] px-6 tablet:py-[14px] tablet:px-[40px] text-xs text-white font-bold hover:bg-blue-500 max-h-8 ${
+                  showMenu == true ? "block" : "tablet:hidden"
+                }`}
+              >
+                Log in
+              </button>
             </div>
           </div>
         </div>
